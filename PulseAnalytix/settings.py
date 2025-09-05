55e77eab5cccc9e8c30a1bf9c50dbd18d05137bc
Raushan <raushan@gmail.com>
2025-09-06 01:33:33 +0530

    signin and login fixed

--- PulseAnalytix/settings.py
+++ PulseAnalytix/settings.py
@@ -80,11 +80,12 @@ WSGI_APPLICATION = "PulseAnalytix.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 # Password validation
--- accounts/forms.py
+++ accounts/forms.py
@@ -11,6 +11,15 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['last_name']
+        if commit:
+            user.save()
+        return user
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
